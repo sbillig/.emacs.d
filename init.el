0daa1b2386d89898293b1fd2dafd29d10d15fcf1
Sean Billig <seanbillig@ebureau.com>
2015-06-02 10:24:09 -0500

    Stuff.

--- init.el
+++ init.el
@@ -83,6 +83,8 @@
   (let ((use-dialog-box nil))
     ad-do-it))
 
+(setq rcirc-default-nick "sbillig")
+
 ;;; Packages
 (package-initialize)
 (setq package-enable-at-startup nil)
@@ -356,7 +358,9 @@
                     "/Users/seanbillig/local/include"
                     "/Users/seanbillig/local/include/libxml2"
                     "/Users/seanbillig/code/modeler_new/external"
+                    "/Users/seanbillig/code/core/dex/src"
                     "/Users/seanbillig/code/core/dex/external"
+                    "/Users/seanbillig/code/core/modelparser/external"
                     "/Users/seanbillig/code/core/newparser/external"
                     "/Users/seanbillig/code/core/dex_newparser/src"
                     "/Users/seanbillig/code/core/dex_newparser/external"
@@ -373,10 +377,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+'(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d"
+                             "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58"
+                             "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365"
+                             default)))
  '(dtrt-indent-min-quality 40.0)
  '(dtrt-indent-mode t nil (dtrt-indent))
  '(dtrt-indent-verbosity 2)
+ '(flycheck-check-syntax-automatically (quote (new-line mode-enabled save)))
  '(ido-enable-flex-matching t)
  '(js3-auto-indent-p t)
  '(js3-consistent-level-indent-inner-bracket nil)
